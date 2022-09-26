aa384c9eb270ab3cf85f4cbd6ad014605b9f00ae
Soumya130498 <soumya.bharadwaj13@gmail.com>
2022-09-26 17:31:07 -0400

    All Requirements done

---
+++ project2/reactUI/db_prj2/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import AppBar from './components/AppBar'
+import Query from './components/Query'
+
+function App() {
+  return (
+    <div className="App">
+    <AppBar/>
+    <Query/>
+   
+    </div>
+  );
+}
+
+export default App;
